2184cdebad499086ae653f3a3b7f972476ca5f61
Chris <hexproofsystems@gmail.com>
2024-11-17 13:47:57 -0600

    refactor(documents): Add item method, improve error handling, docstrings, and type annotations
    Signed-off-by: Chris <hexproofsystems@gmail.com>

--- photoshop/api/_documents.py
+++ photoshop/api/_documents.py
@@ -1,22 +1,52 @@
+# Import built-in modules
+from ctypes import ArgumentError
+from typing import Union
+
+# Import third-party modules
+from _ctypes import COMError
+
 # Import local modules
 from photoshop.api._core import Photoshop
 from photoshop.api._document import Document
 from photoshop.api.enumerations import BitsPerChannelType
 from photoshop.api.enumerations import DocumentFill
 from photoshop.api.enumerations import NewDocumentMode
-from photoshop.api.errors import PhotoshopPythonAPIError
 
 
 # pylint: disable=too-many-public-methods, too-many-arguments
 class Documents(Photoshop):
     """The collection of open documents."""
 
-    def __init__(self, parent):
-        super().__init__(parent=parent)
-        self._flag_as_method("add")
+    app_methods = ["add"]
+
+    def __getitem__(self, item: Union[int, str]) -> Document:
+        """Retrieve a Document from the Documents collection with the provided name or index.
+
+        Args:
+            item (Union[int, str]): The index within the Documents collection or name of the
+                Document to look for.
+
+        Returns:
+            A Document object.
+
+        Raises:
+            IndexError: If item was provided as an integer and is out of range of collection array.
+            KeyError: If item was provided as a string and the Document name was not found.
+        """
+        if isinstance(item, int):
+            return self.item(item)
+        return self.getByName(item)
+
+    def __iter__(self) -> Document:
+        for doc in self.app:
+            yield Document(doc)
 
     def __len__(self) -> int:
         return self.length
+
+    @property
+    def length(self) -> int:
+        return len(list(self.app))
 
     def add(
         self,
@@ -59,27 +89,39 @@ class Documents(Photoshop):
                 pixelAspectRatio,
                 bitsPerChannel,
                 colorProfileName,
-            )
+            ),
         )
 
-    def __iter__(self) -> Document:
-        for doc in self.app:
-            self.adobe.activeDocument = doc
-            yield Document(doc)
+    def item(self, index: int) -> Document:
+        """Retrieve an item from the Documents collection by index in the array.
 
-    def __getitem__(self, item) -> Document:
+        Args:
+            index: Index in the array.
+
+        Returns:
+            A Document object.
+
+        Raises:
+            IndexError: If index provided is out of range.
+        """
         try:
-            return Document(self.app[item])
-        except IndexError:
-            raise PhotoshopPythonAPIError("Currently Photoshop did not find Documents.")
+            return Document(self.app.item(index + 1))
+        except (COMError, IndexError):
+            raise IndexError(f"Index [{index}] out of range in Documents collection.")
 
-    @property
-    def length(self) -> int:
-        return len(list(self.app))
+    def getByName(self, name: str) -> Document:
+        """Retrieve a Document from the Documents collection by name.
 
-    def getByName(self, document_name: str) -> Document:
-        """Get document by given document name."""
-        for doc in self.app:
-            if doc.name == document_name:
-                return Document(doc)
-        raise PhotoshopPythonAPIError(f'Could not find a document named "{document_name}"')
+        Args:
+            name (str): The name of the Document to look for.
+
+        Returns:
+            A Document object.
+
+        Raises:
+            KeyError: If no document with the given name exists.
+        """
+        try:
+            return Document(self.app[name])
+        except (ArgumentError, COMError, KeyError):
+            raise KeyError(f"No document found with name '{name}'.")
