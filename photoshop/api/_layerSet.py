72ed54c166ea847fffb051e7a363a6ffb955775a
Chris <hexproofsystems@gmail.com>
2024-11-17 15:07:08 -0600

    fix(layerSet): Make parent property functional, update base collection functionality, update duplicate and move methods with workarounds for behavior not supported by COM, improvements to docstrings, type annotations, and error handling
    Signed-off-by: Chris <hexproofsystems@gmail.com>

--- photoshop/api/_layerSet.py
+++ photoshop/api/_layerSet.py
@@ -1,33 +1,58 @@
+# Import built-in modules
+from __future__ import annotations
+from typing import Union, TYPE_CHECKING, Optional
+
+# Import third-party modules
+from _ctypes import COMError
+from comtypes.client.lazybind import Dispatch
+
 # Import local modules
 from photoshop.api._artlayer import ArtLayer
 from photoshop.api._artlayers import ArtLayers
 from photoshop.api._core import Photoshop
 from photoshop.api._layers import Layers
 from photoshop.api.enumerations import AnchorPosition
+from photoshop.api.enumerations import ElementPlacement
 from photoshop.api.enumerations import BlendMode
+if TYPE_CHECKING:
+    from photoshop.api._document import Document
+    from photoshop.api._layerSets import LayerSets
 
 
 class LayerSet(Photoshop):
     """A group of layer objects, which can include art layer objects and other (nested) layer set objects.
 
     A single command or set of commands manipulates all layers in a layer set object.
-
     """
 
-    def __init__(self, parent):
-        super().__init__(parent=parent)
-        self._flag_as_method(
-            "merge",
-            "duplicate",
-            "add",
-            "delete",
-            "link",
-            "move",
-            "resize",
-            "rotate",
-            "translate",
-            "unlink",
-        )
+    app_methods = ["merge", "duplicate", "add", "delete", "link", "move", "resize", "rotate", "translate", "unlink"]
+
+    def __getitem__(self, item: Union[int, str]) -> Union[ArtLayer, LayerSet]:
+        """Retrieve an ArtLayer or LayerSet from this LayerSet by name (if provided
+            as a string) or index (if provided as an integer) within the layers array.
+
+        Args:
+            item: The name of the layer to retrieve or index within the layers array.
+
+        Returns:
+            ArtLayer or LayerSet object.
+
+        Raises:
+            KeyError: If no ArtLayer or LayerSet is found with a provided
+                name or index.
+        """
+        return self.layers[item]
+
+    def __iter__(self):
+        for layer in self.layers:
+            yield layer
+
+    def __len__(self):
+        return self.length
+
+    @property
+    def _layers(self) -> list[Dispatch]:
+        return list(self.layers._layers)
 
     @property
     def allLocked(self):
@@ -63,11 +88,14 @@ class LayerSet(Photoshop):
         return Layers(self.app.layers)
 
     @property
-    def layerSets(self):
+    def layerSets(self) -> LayerSets:
         # pylint: disable=import-outside-toplevel
         from ._layerSets import LayerSets
-
         return LayerSets(self.app.layerSets)
+
+    @property
+    def length(self) -> int:
+        return len(self._layers)
 
     @property
     def linkedLayers(self):
@@ -93,33 +121,113 @@ class LayerSet(Photoshop):
         self.app.opacity = value
 
     @property
-    def parent(self):
-        return self.app.parent
+    def parent(self) -> Union['Document', LayerSet]:
+        """The parent `Document` or `LayerSet` containing this `LayerSet`."""
+        _parent = self.app.parent
+        try:
+            # Parent is a Document
+            _ = _parent.path
+            from photoshop.api._document import Document
+            return Document(_parent)
+        except (COMError, NameError, OSError):
+            # Parent is a LayerSet
+            return LayerSet(_parent)
+
+    @parent.setter
+    def parent(self, parent: Union['Document', 'LayerSet']):
+        """Set the object’s parent container."""
+        if parent.__class__.__name__ == 'Document':
+            # Move to another Document
+            _new = self.duplicate(parent, ElementPlacement.PlaceAtBeginning)
+            self.remove()
+            self.app = _new.app
+        else:
+            # Move to another LayerSet
+            self.move(parent, ElementPlacement.PlaceInside)
 
     @property
-    def visible(self):
+    def visible(self) -> bool:
         return self.app.visible
 
     @visible.setter
-    def visible(self, value):
+    def visible(self, value: bool):
         self.app.visible = value
 
-    def duplicate(self, relativeObject=None, insertionLocation=None):
+    def duplicate(
+        self, relativeObject: Optional[Union[ArtLayer, 'Document', 'LayerSet']] = None,
+        insertionLocation: Optional[ElementPlacement] = None
+    ):
+        """Duplicate this `LayerSet` and place the duplicate relative to another `ArtLayer`,
+            `Document`, or `LayerSet`.
+
+        Note:
+            LayerSet cannot be moved inside a target LayerSet, so we create a
+                temporary layer for positioning.
+
+        Args:
+            relativeObject: An ArtLayer, Document, or LayerSet object to position the
+                duplicate object relative to.
+            insertionLocation: The placement strategy of the duplicate object relative to
+                the relativeObject. Should be provided as an ElementPlacement enum.
+
+        Returns:
+            The duplicate `LayerSet` object.
+        """
+        if isinstance(relativeObject, LayerSet) and ElementPlacement not in [
+            ElementPlacement.PlaceBefore, ElementPlacement.PlaceAfter
+        ]:
+            _temp = relativeObject.add()
+            if insertionLocation == ElementPlacement.PlaceAtEnd:
+                _temp.move(relativeObject, ElementPlacement.PlaceAtEnd)
+            _duplicate = LayerSet(self.app.duplicate(_temp.app, ElementPlacement.PlaceBefore))
+            _temp.remove()
+            return _duplicate
         return LayerSet(self.app.duplicate(relativeObject, insertionLocation))
 
     def link(self, with_layer):
         self.app.link(with_layer)
 
-    def add(self):
-        """Adds an element."""
-        self.app.add()
+    def add(self, layer_set: bool = False):
+        """Adds a layer object to this `LayerSet`.
+
+        Args:
+            layer_set: If True, add and return a new `LayerSet` object. Otherwise, add
+                and return a new `ArtLayer`.
+        """
+        if layer_set:
+            return self.layerSets.add()
+        return self.artLayers.add()
 
     def merge(self) -> ArtLayer:
         """Merges the layer set."""
         return ArtLayer(self.app.merge())
 
-    def move(self, relativeObject, insertionLocation):
-        self.app.move(relativeObject, insertionLocation)
+    def move(
+        self, relativeObject: Optional[Union[ArtLayer, 'Document', 'LayerSet']] = None,
+        insertionLocation: Optional[ElementPlacement] = None
+    ) -> None:
+        """Move this `LayerSet` relative to another `ArtLayer`, `Document`, or `LayerSet`.
+
+        Note:
+            LayerSet cannot be moved inside a target LayerSet, so we create a
+                temporary layer for positioning.
+
+        Args:
+            relativeObject: An ArtLayer, Document, or LayerSet object to position this
+                object relative to.
+            insertionLocation: The placement strategy of this object relative to the
+                relativeObject. Should be provided as an ElementPlacement enum.
+        """
+        if isinstance(relativeObject, LayerSet) and ElementPlacement not in [
+            ElementPlacement.PlaceBefore, ElementPlacement.PlaceAfter
+        ]:
+            _temp = relativeObject.add()
+            if insertionLocation == ElementPlacement.PlaceAtEnd:
+                _temp.move(relativeObject, ElementPlacement.PlaceAtEnd)
+            self.app.move(_temp.app, ElementPlacement.PlaceBefore)
+            _temp.remove()
+        else:
+            self.app.move(relativeObject, insertionLocation)
 
     def remove(self):
         """Remove this layer set from the document."""
@@ -138,7 +246,3 @@ class LayerSet(Photoshop):
     def unlink(self):
         """Unlinks the layer set."""
         self.app.unlink()
-
-    def __iter__(self):
-        for layer in self.app:
-            yield layer
