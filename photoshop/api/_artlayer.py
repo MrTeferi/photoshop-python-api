dbe1bee8d8dc6765993c1a158087a6ef02cce862
Chris <hexproofsystems@gmail.com>
2024-11-17 15:09:22 -0600

    fix(artLayer): Make parent property functional, improve docstrings, type annotations and error handling
    Signed-off-by: Chris <hexproofsystems@gmail.com>

--- photoshop/api/_artlayer.py
+++ photoshop/api/_artlayer.py
@@ -1,64 +1,67 @@
 # Import built-in modules
-from typing import Any
+from contextlib import suppress
+from typing import Union, TYPE_CHECKING, Optional
+
+# Import third-party modules
+from _ctypes import COMError
 
 # Import local modules
 from photoshop.api._core import Photoshop
+from photoshop.api.enumerations import ElementPlacement
 from photoshop.api.enumerations import LayerKind
 from photoshop.api.enumerations import RasterizeType
 from photoshop.api.text_item import TextItem
+if TYPE_CHECKING:
+    from photoshop.api._document import Document
+    from photoshop.api._layerSet import LayerSet
 
 
 # pylint: disable=too-many-public-methods, too-many-arguments
 class ArtLayer(Photoshop):
     """An object within a document that contains the visual elements of the image
-
     (equivalent to a layer in the Adobe Photoshop application).
-
     """
 
-    def __init__(self, parent: Any = None):
-        super().__init__(parent=parent)
-        self._flag_as_method(
-            "add",
-            "adjustBrightnessContrast",
-            "adjustColorBalance",
-            "adjustCurves",
-            "adjustLevels",
-            "applyAddNoise",
-            "applyAverage",
-            "applyBlur",
-            "applyBlurMore",
-            "applyClouds",
-            "applyCustomFilter",
-            "applyDeInterlace",
-            "applyDespeckle",
-            "applyDifferenceClouds",
-            "applyDiffuseGlow",
-            "applyDisplace",
-            "applyDustAndScratches",
-            "applyGaussianBlur",
-            "applyGlassEffect",
-            "applyHighPass",
-            "applyLensBlur",
-            "applyLensFlare",
-            "applyMaximum",
-            "applyMedianNoise",
-            "applyMinimum",
-            "applyMotionBlur",
-            "applyNTSC",
-            "applyOceanRipple",
-            "applyOffset",
-            "applyPinch",
-            "delete",
-            "duplicate",
-            "invert",
-            "link",
-            "merge",
-            "move",
-            "posterize",
-            "rasterize",
-            "unlink",
-        )
+    app_methods = [
+        "adjustBrightnessContrast",
+        "adjustColorBalance",
+        "adjustCurves",
+        "adjustLevels",
+        "applyAddNoise",
+        "applyAverage",
+        "applyBlur",
+        "applyBlurMore",
+        "applyClouds",
+        "applyCustomFilter",
+        "applyDeInterlace",
+        "applyDespeckle",
+        "applyDifferenceClouds",
+        "applyDiffuseGlow",
+        "applyDisplace",
+        "applyDustAndScratches",
+        "applyGaussianBlur",
+        "applyGlassEffect",
+        "applyHighPass",
+        "applyLensBlur",
+        "applyLensFlare",
+        "applyMaximum",
+        "applyMedianNoise",
+        "applyMinimum",
+        "applyMotionBlur",
+        "applyNTSC",
+        "applyOceanRipple",
+        "applyOffset",
+        "applyPinch",
+        "delete",
+        "duplicate",
+        "invert",
+        "link",
+        "merge",
+        "move",
+        "posterize",
+        "rasterize",
+        "unlink",
+    ]
 
     @property
     def allLocked(self):
@@ -182,51 +185,63 @@ class ArtLayer(Photoshop):
         return round(self.app.opacity)
 
     @opacity.setter
-    def opacity(self, value):
+    def opacity(self, value: Union[int, float]):
+        """Set the master opacity of the layer.
+
+        Args:
+            value: Integer or float from 0-100.
+        """
         self.app.opacity = value
 
     @property
-    def parent(self):
-        """The object’s container."""
-        return self.app.parent
+    def parent(self) -> Union['Document', 'LayerSet']:
+        """The parent `Document` or `LayerSet` containing this `ArtLayer`."""
+        _parent = self.app.parent
+        try:
+            # Parent is a Document
+            _ = _parent.path
+            from photoshop.api._document import Document
+            return Document(_parent)
+        except (COMError, NameError, OSError):
+            # Parent is a LayerSet
+            from photoshop.api._layerSet import LayerSet
+            return LayerSet(_parent)
 
     @parent.setter
-    def parent(self, value):
+    def parent(self, parent: Union['Document', 'LayerSet']):
         """Set the object’s container."""
-        self.app.parent = value
+        if parent.__class__.__name__ == 'Document':
+            _new = self.duplicate(parent, ElementPlacement.PlaceAtBeginning)
+            self.remove()
+            self.app = _new.app
+        else:
+            self.move(parent, ElementPlacement.PlaceInside)
 
     @property
-    def pixelsLocked(self):
+    def pixelsLocked(self) -> bool:
         """If true, the pixels in the layer’s image cannot be edited."""
         return self.app.pixelsLocked
 
     @pixelsLocked.setter
-    def pixelsLocked(self, value):
+    def pixelsLocked(self, value: bool):
         self.app.pixelsLocked = value
 
     @property
-    def positionLocked(self):
+    def positionLocked(self) -> bool:
         """bool: If true, the pixels in the layer’s image cannot be moved
         within the layer."""
         return self.app.positionLocked
 
     @positionLocked.setter
-    def positionLocked(self, value):
+    def positionLocked(self, value: bool):
         self.app.positionLocked = value
 
     @property
     def textItem(self) -> TextItem:
         """The text that is associated with the layer. Valid only when ‘kind’
-        is text layer.
-
-        returns:
-            TextItem:
-        """
+            is text layer. Note that some documentation sources insist this is a writable
+            property, however with COM automation this doesn't appear to be possible."""
         return TextItem(self.app.textItem)
-
-    @textItem.setter
-    def textItem(self, value):
-        self.app.textItem = value
 
     @property
     def transparentPixelsLocked(self):
@@ -264,10 +279,7 @@ class ArtLayer(Photoshop):
     def length(self):
         return len(list(self.app))
 
-    def add(self):
-        return self.app.add()
-
-    def adjustBrightnessContrast(self, brightness, contrast):
+    def adjustBrightnessContrast(self, brightness, contrast) -> None:
         """Adjusts the brightness and contrast.
 
         Args:
@@ -283,7 +295,7 @@ class ArtLayer(Photoshop):
         midtones,
         highlights,
         preserveLuminosity,
-    ):
+    ) -> None:
         """Adjusts the color balance of the layer’s component channels.
 
         Args:
@@ -309,7 +321,7 @@ class ArtLayer(Photoshop):
             preserveLuminosity,
         )
 
-    def adjustCurves(self, curveShape):
+    def adjustCurves(self, curveShape) -> None:
         """Adjusts the tonal range of the selected channel using up to fourteen
         points.
 
@@ -331,7 +343,7 @@ class ArtLayer(Photoshop):
         inputRangeGamma,
         outputRangeStart,
         outputRangeEnd,
-    ):
+    ) -> None:
         """Adjusts levels of the selected channels.
 
         Args:
@@ -340,9 +352,6 @@ class ArtLayer(Photoshop):
             inputRangeGamma:
             outputRangeStart:
             outputRangeEnd:
-
-        Returns:
-
         """
         return self.app.adjustLevels(
             inputRangeStart,
@@ -352,10 +361,10 @@ class ArtLayer(Photoshop):
             outputRangeEnd,
         )
 
-    def applyAddNoise(self, amount, distribution, monochromatic):
+    def applyAddNoise(self, amount, distribution, monochromatic) -> None:
         return self.app.applyAddNoise(amount, distribution, monochromatic)
 
-    def applyDiffuseGlow(self, graininess, amount, clear_amount):
+    def applyDiffuseGlow(self, graininess, amount, clear_amount) -> None:
         """Applies the diffuse glow filter.
 
         Args:
@@ -368,34 +377,34 @@ class ArtLayer(Photoshop):
         """
         return self.app.applyDiffuseGlow(graininess, amount, clear_amount)
 
-    def applyAverage(self):
+    def applyAverage(self) -> None:
         """Applies the average filter."""
         return self.app.applyAverage()
 
-    def applyBlur(self):
+    def applyBlur(self) -> None:
         """Applies the blur filter."""
         return self.app.applyBlur()
 
-    def applyBlurMore(self):
+    def applyBlurMore(self) -> None:
         """Applies the blur more filter."""
         return self.app.applyBlurMore()
 
-    def applyClouds(self):
+    def applyClouds(self) -> None:
         """Applies the clouds filter."""
         return self.app.applyClouds()
 
-    def applyCustomFilter(self, characteristics, scale, offset):
+    def applyCustomFilter(self, characteristics, scale, offset) -> None:
         """Applies the custom filter."""
         return self.app.applyCustomFilter(characteristics, scale, offset)
 
-    def applyDeInterlace(self, eliminateFields, createFields):
+    def applyDeInterlace(self, eliminateFields, createFields) -> None:
         """Applies the de-interlace filter."""
         return self.app.applyDeInterlace(eliminateFields, createFields)
 
-    def applyDespeckle(self):
+    def applyDespeckle(self) -> None:
         return self.app.applyDespeckle()
 
-    def applyDifferenceClouds(self):
+    def applyDifferenceClouds(self) -> None:
         """Applies the difference clouds filter."""
         return self.app.applyDifferenceClouds()
 
@@ -406,7 +415,7 @@ class ArtLayer(Photoshop):
         displacementType,
         undefinedAreas,
         displacementMapFile,
-    ):
+    ) -> None:
         """Applies the displace filter."""
         return self.app.applyDisplace(
             horizontalScale,
@@ -416,11 +425,11 @@ class ArtLayer(Photoshop):
             displacementMapFile,
         )
 
-    def applyDustAndScratches(self, radius, threshold):
+    def applyDustAndScratches(self, radius, threshold) -> None:
         """Applies the dust and scratches filter."""
         return self.app.applyDustAndScratches(radius, threshold)
 
-    def applyGaussianBlur(self, radius):
+    def applyGaussianBlur(self, radius) -> None:
         """Applies the gaussian blur filter."""
         return self.app.applyGaussianBlur(radius)
 
@@ -432,7 +441,7 @@ class ArtLayer(Photoshop):
         invert,
         texture,
         textureFile,
-    ):
+    ) -> None:
         return self.app.applyGlassEffect(
             distortion,
             smoothness,
@@ -442,7 +451,7 @@ class ArtLayer(Photoshop):
             textureFile,
         )
 
-    def applyHighPass(self, radius):
+    def applyHighPass(self, radius) -> None:
         """Applies the high pass filter."""
         return self.app.applyHighPass(radius)
 
@@ -460,7 +469,7 @@ class ArtLayer(Photoshop):
         amount,
         distribution,
         monochromatic,
-    ):
+    ) -> None:
         """Apply the lens blur filter."""
         return self.app.applyLensBlur(
             source,
@@ -477,44 +486,58 @@ class ArtLayer(Photoshop):
             monochromatic,
         )
 
-    def applyLensFlare(self, brightness, flareCenter, lensType):
+    def applyLensFlare(self, brightness, flareCenter, lensType) -> None:
         return self.app.applyLensFlare(brightness, flareCenter, lensType)
 
-    def applyMaximum(self, radius):
-        self.app.applyMaximum(radius)
+    def applyMaximum(self, radius) -> None:
+        return self.app.applyMaximum(radius)
 
-    def applyMedianNoise(self, radius):
-        self.app.applyMedianNoise(radius)
+    def applyMedianNoise(self, radius) -> None:
+        return self.app.applyMedianNoise(radius)
 
-    def applyMinimum(self, radius):
-        self.app.applyMinimum(radius)
+    def applyMinimum(self, radius) -> None:
+        return self.app.applyMinimum(radius)
 
-    def applyMotionBlur(self, angle, radius):
-        self.app.applyMotionBlur(angle, radius)
+    def applyMotionBlur(self, angle, radius) -> None:
+        return self.app.applyMotionBlur(angle, radius)
 
-    def applyNTSC(self):
-        self.app.applyNTSC()
+    def applyNTSC(self) -> None:
+        return self.app.applyNTSC()
 
-    def applyOceanRipple(self, size, magnitude):
-        self.app.applyOceanRipple(size, magnitude)
+    def applyOceanRipple(self, size, magnitude) -> None:
+        return self.app.applyOceanRipple(size, magnitude)
 
-    def applyOffset(self, horizontal, vertical, undefinedAreas):
-        self.app.applyOffset(horizontal, vertical, undefinedAreas)
+    def applyOffset(self, horizontal, vertical, undefinedAreas) -> None:
+        return self.app.applyOffset(horizontal, vertical, undefinedAreas)
 
-    def applyPinch(self, amount):
-        self.app.applyPinch(amount)
+    def applyPinch(self, amount) -> None:
+        return self.app.applyPinch(amount)
 
-    def remove(self):
-        """Removes this layer from the document."""
-        self.app.delete()
+    def remove(self) -> None:
+        """Removes this layer from the document and deletes the instance."""
+        # Note: Always raises an exception, even when successfully deleted.
+        with suppress(Exception):
+            return self.app.delete()
 
-    def rasterize(self, target: RasterizeType):
+    def rasterize(self, target: RasterizeType) -> None:
         self.app.rasterize(target)
 
-    def posterize(self, levels):
+    def posterize(self, levels) -> None:
         self.app.posterize(levels)
 
-    def move(self, relativeObject, insertionLocation):
+    def move(
+        self,
+        relativeObject: Optional[Union['ArtLayer', 'Document', 'LayerSet']] = None,
+        insertionLocation: Optional[ElementPlacement] = None
+    ) -> None:
+        """Move this `ArtLayer` relative to another `ArtLayer`, `Document`, or `LayerSet`.
+
+        Args:
+            relativeObject: An ArtLayer, Document, or LayerSet object to position this
+                object relative to.
+            insertionLocation: The placement strategy of this object relative to the
+                relativeObject. Should be provided as an ElementPlacement enum.
+        """
         self.app.move(relativeObject, insertionLocation)
 
     def merge(self):
@@ -530,5 +553,20 @@ class ArtLayer(Photoshop):
     def invert(self):
         self.app.invert()
 
-    def duplicate(self, relativeObject=None, insertionLocation=None):
+    def duplicate(
+        self, relativeObject: Optional[Union['ArtLayer', 'Document', 'LayerSet']] = None,
+        insertionLocation: Optional[ElementPlacement] = None
+    ):
+        """Duplicate this `ArtLayer` and place the duplicate relative to another `ArtLayer`,
+            `Document`, or `LayerSet`.
+
+        Args:
+            relativeObject: An ArtLayer, Document, or LayerSet object to position the
+                duplicate object relative to.
+            insertionLocation: The placement strategy of the duplicate object relative to
+                the relativeObject. Should be provided as an ElementPlacement enum.
+
+        Returns:
+            The duplicate `ArtLayer` object.
+        """
         return ArtLayer(self.app.duplicate(relativeObject, insertionLocation))
