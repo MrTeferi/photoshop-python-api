c071b3b0d571857332b9c047f531f8a7d1a86633
Chris <hexproofsystems@gmail.com>
2024-11-17 13:06:50 -0600

    perf(layerSets): Improve performance of getByIndex, getByName. Ensure __iter__ yields LayerSet objects. Improve docstrings and type annotations
    Signed-off-by: Chris <hexproofsystems@gmail.com>

--- photoshop/api/_layerSets.py
+++ photoshop/api/_layerSets.py
@@ -1,4 +1,5 @@
 # Import third-party modules
+from _ctypes import COMError
 from comtypes import ArgumentError
 
 # Import local modules
@@ -10,20 +11,14 @@ from photoshop.api.errors import PhotoshopPythonAPIError
 class LayerSets(Photoshop):
     """The layer sets collection in the document."""
 
-    def __init__(self, parent):
-        super().__init__(parent=parent)
-        self._flag_as_method(
-            "add",
-            "item",
-            "removeAll",
-        )
+    app_methods = ["add", "item", "removeAll"]
 
     def __len__(self):
         return self.length
 
     def __iter__(self):
         for layer_set in self.app:
-            yield layer_set
+            yield LayerSet(layer_set)
 
     def __getitem__(self, key: str):
         """Access a given LayerSet using dictionary key lookup."""
@@ -45,18 +40,30 @@ class LayerSets(Photoshop):
         return LayerSet(self.app.add())
 
     def item(self, index: int) -> LayerSet:
-        return LayerSet(self.app.item(index))
+        """Retrieves an item in this LayerSets collection by its index in the array.
+
+        Note:
+            Indexes start at 1 in Photoshop, we correct for this by adding 1 to the index
+                provided so there's no confusion with Python conventions.
+
+        Args:
+            index: The index of the item to retrieve within the collection.
+
+        Raises:
+            IndexError: If the index provided is out of range.
+        """
+        try:
+            return LayerSet(self.app.item(index + 1))
+        except (IndexError, COMError) as e:
+            raise IndexError(f"Index [{index}] is outside the range of this layer collection.") from e
 
     def removeAll(self):
         self.app.removeAll()
 
     def getByIndex(self, index: int):
         """Access LayerSet using list index lookup."""
-        return LayerSet(self._layerSets[index])
+        return self.item(index)
 
     def getByName(self, name: str) -> LayerSet:
         """Get the first element in the collection with the provided name."""
-        for layer in self.app:
-            if name == layer.name:
-                return LayerSet(layer)
-        raise PhotoshopPythonAPIError(f'Could not find a LayerSet named "{name}"')
+        return self[name]
