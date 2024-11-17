de86e9e179eb11442bf21f3c19fff35d3fa7ead9
Chris <hexproofsystems@gmail.com>
2024-11-17 14:46:23 -0600

    refactor(notifiers): Bring basic functionality in line with other collection classes, improve docstrings and type annotations
    Signed-off-by: Chris <hexproofsystems@gmail.com>

--- photoshop/api/_notifiers.py
+++ photoshop/api/_notifiers.py
@@ -9,48 +9,60 @@ Examples:
     ```
 
 """
-
 # Import built-in modules
-from typing import Any
+from typing import Any, Iterator
 from typing import Optional
 
+# Import third-party modules
+from comtypes import ArgumentError
+from comtypes.client.lazybind import Dispatch
+
 # Import local modules
+from photoshop.api.errors import PhotoshopPythonAPIError
 from photoshop.api._core import Photoshop
 from photoshop.api._notifier import Notifier
 
 
 class Notifiers(Photoshop):
-    """The `notifiers` currently configured (in the Scripts Events Manager menu in the application)."""
+    """An array collection of each `Notifier` currently configured (in the Scripts Events Manager
+        menu in Photoshop)."""
 
-    def __init__(self, parent: Optional[Any] = None):
-        super().__init__(parent=parent)
-        self._flag_as_method(
-            "add",
-            "removeAll",
-        )
+    app_methods = ["add", "removeAll"]
 
-    @property
-    def _notifiers(self) -> list:
-        return [n for n in self.app]
-
-    def __len__(self):
+    def __len__(self) -> int:
         return self.length
 
-    def __iter__(self):
-        for app in self.app:
-            yield app
+    def __iter__(self) -> Iterator[Notifier]:
+        for notifier in self.app:
+            yield Notifier(notifier)
 
-    def __getitem__(self, item):
-        return self._notifiers[item]
+    def __getitem__(self, item: int) -> Notifier:
+        """Return a Notifier from the collection by index in the array."""
+        try:
+            return Notifier(self._notifiers[item])
+        except ArgumentError:
+            raise PhotoshopPythonAPIError(f'Could not find a Notifier with index {item}')
 
     @property
-    def length(self):
+    def _notifiers(self) -> list[Dispatch]:
+        """Private property that returns a list containing a Dispatch object for each
+            `Notifier` in this collection."""
+        return list(self.app)
+
+    @property
+    def length(self) -> int:
+        """The number of `Notifier` objects in this collection."""
         return len(self._notifiers)
 
-    def add(self, event, event_file: Optional[Any] = None, event_class: Optional[Any] = None) -> Notifier:
+    def add(
+        self, event: str, event_file: Optional[Any] = None,
+        event_class: Optional[Any] = None
+    ) -> Notifier:
+        """Add a `Notifier` to the collection."""
         self.parent.notifiersEnabled = True
         return Notifier(self.app.add(event, event_file, event_class))
 
     def removeAll(self):
+        """Remove all `Notifier` objects from the collection."""
         self.app.removeAll()
         self.parent.notifiersEnabled = False
